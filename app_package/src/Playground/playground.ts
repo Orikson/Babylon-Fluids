306bf0e4a1f71957a58ea0d10068f8a72588cf7a
Orikson <42471346+Orikson@users.noreply.github.com>
2022-10-05 00:03:17 -0700

    ar support or lack of

--- app_package/src/Playground/playground.ts
+++ app_package/src/Playground/playground.ts
@@ -5,18 +5,49 @@ import { ObjectCollisionMatrix } from "cannon-es";
 class Playground {
     scene: BABYLON.Scene;
 
+    // xr enabled
+    XRenabled: boolean;
+    sessionManager: BABYLON.WebXRSessionManager;
+    
     constructor(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
         this.scene = new BABYLON.Scene(engine);
+        this.sessionManager = new BABYLON.WebXRSessionManager(this.scene);
+        this.XRenabled = false;
+        
         this.CreateScene(engine, canvas);
     }
 
     async CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
-        var objPos = new BABYLON.Vector3(0, 0, 0);
+        // xr manager
+        this.XRenabled = await BABYLON.WebXRSessionManager.IsSessionSupportedAsync('immersive-ar');
+        var camera;
+        
+        // if XR is enabled for immersive-ar (which only occurs on very specific devices such as the Hololens 2), load immmersive ar experience
+        if (this.XRenabled) {
+            this.sessionManager.initializeSessionAsync("immersive-ar");
+            
+            const referenceSpace = this.sessionManager.setReferenceSpaceTypeAsync();
+            const renderTarget = this.sessionManager.getWebXRRenderTarget();
+            const xrWebGLLayer = renderTarget.initializeXRLayerAsync(this.sessionManager.session);
 
-        // This creates and positions a free camera (non-mesh)
-        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), this.scene);
-        camera.setTarget(BABYLON.Vector3.Zero());
-        camera.attachControl(canvas, true);
+            // here we add XR support
+            const xr = await this.scene.createDefaultXRExperienceAsync({
+                uiOptions: {
+                    sessionMode: "immersive-ar",
+                },
+            });
+
+            // This creates and positions a free camera (non-mesh)
+            camera = new BABYLON.WebXRCamera("webxrcamera", this.scene, this.sessionManager);
+            camera.setTarget(BABYLON.Vector3.Zero());
+            camera.attachControl(canvas, true);
+        } else {
+            camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), this.scene);
+            camera.setTarget(BABYLON.Vector3.Zero());
+            camera.attachControl(canvas, true);
+        }
+        
+        var objPos = new BABYLON.Vector3(0, 0, 0);
 
         var lightShader = new BABYLON.ShaderMaterial("shader", this.scene, "./liquids", {
             attributes: ["position", "normal", "uv"],
@@ -37,13 +68,6 @@ class Playground {
 
         // setup environment
         //const env = this.scene.createDefaultEnvironment();
-      
-        // here we add XR support
-        const xr = await this.scene.createDefaultXRExperienceAsync({
-            uiOptions: {
-                sessionMode: "immersive-ar",
-            },
-        });
     }
 }
 
@@ -53,5 +77,9 @@ export function CreatePlayground(engine: BABYLON.Engine, canvas: HTMLCanvasEleme
 }
 
 export function RenderLoop(playground: Playground) {
-    playground.scene.render();
+    if (playground.XRenabled) {
+        playground.sessionManager.runXRRenderLoop();
+    } else {
+        playground.scene.render();
+    }
 }
